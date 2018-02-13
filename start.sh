a356ab18b7c7d9cf07d179f910282e96445c5b48
Zajs <aga.zajs@gmail.com>
2018-02-13 18:05:33 +0700

    start.sh fix

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-
 #!/usr/bin/env sh
 
-[ -z "$JAVA_XMX" ] && JAVA_XMX="256M"
-
-JAVA_OPTS="-Xmx$JAVA_XMX -Xms$JAVA_XMX"
-
-java $JAVA_OPTS -cp "/app/app.jar:/app/lib/*" io.hydrosphere.serving.grpc_spark.Main
+java -cp "/app/app.jar:/app/lib/*" io.hydrosphere.serving.grpc_spark.Main
